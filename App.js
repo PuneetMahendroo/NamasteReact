880109292777c104c1df4171afabd2dfdba98ff9
Puneet Mahendroo <mahendroopuneet@gmail.com>
2025-06-27 22:11:15 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement("h1",{
+    id:"heading"
+},"Namaste World from React");
+
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"},
+    [    React.createElement("h1",{},"This is a h1 tag")
+        ,React.createElement("h2",{},"This is a h2 tag")]
+    ),
+    React.createElement("div",{id:"child2"},
+    [    React.createElement("h1",{},"This is a h1 tag")
+        ,React.createElement("h2",{},"This is a h2 tag")]
+    ),    
+]
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
